70d41592c830c54900da15fd83fdb0e03ae3c66c
Rishmika Sandanu <106467954+ImRishmika@users.noreply.github.com>
2022-09-24 16:57:37 +0530

    Update membercountermeta.py

--- membercountermeta.py
+++ membercountermeta.py
@@ -13,13 +13,13 @@ from texts.texts_teletips import *
 
 MemberCounterMeta = Client(
     name = "membercountermeta",
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
-    session_string = os.environ["SESSION_STRING"]
+    api_id = int(os.environ["14055090"]),
+    api_hash = os.environ["a46f7b439d0afa45b7a69fc450f754e9"],
+    session_string = os.environ["1BVtsOIkBuy3uFjMYzr5t6QzTJ_Jx3wdEloCwDopspcyk-VT2muCoDs0sUCRN6BS-5OfIVFMlgUZ_5O4xETTgy4MJs2L9LokmXZ2lWxvgr0kfdSxFvX5P6vW_X6d9tgy1X9eGnKNZDJLLOb35dLkI8opl1KBXKtAVhBx8Tblqq27HQZ96Et4TlamQcdIHHLadtl49dVsdUiScFQAUWpXujMoblMUwdiPq1IcHCU6Az-gc0usAWh_SniNHZF0w1P2hH3UGiKoEsyEqOnCnN6hLY2tKPOKYNLT2KeOsN4bOZkvMJ_w4awhPVLI9Utmz0IgCXbqbF6AIoo2F4ZPKvPnJB4zS4PHdLvI="]
 )
-CHANNEL_OR_GROUP_LIST = [i.strip() for i in os.environ.get("CHANNEL_OR_GROUP_LIST").split(' ')]
-CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
-MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+CHANNEL_OR_GROUP_LIST = [i.strip() for i in os.environ.get("-1001742371821").split(' ')]
+CHANNEL_OR_GROUP_ID = int(os.environ["-1001742371821"])
+MESSAGE_ID = int(os.environ["-1001742371821"])
 
 print(text_1)
 async def main_MemberCounterMeta():
@@ -27,18 +27,18 @@ async def main_MemberCounterMeta():
         try:
             while True:
                 print(text_2)
-                edit_message_text_teletips = "**📈 | Real-Time Member Counter** [ᵐᵉᵗᵃ](https://github.com/teletips/MemberCounterMeta#:~:text=Versions-,1.0%20Meta,-Credits)"
+                edit_message_text_teletips = "**📈 | Emo Member Counter** ☘ The Simple And Fast Member Counter On Telegram"
                 for CHANNEL_OR_GROUP in CHANNEL_OR_GROUP_LIST:
                     try:
                         get_chat_teletips = await MemberCounterMeta.get_chat(int(CHANNEL_OR_GROUP))   
                         if get_chat_teletips.type == "channel":
-                            edit_message_text_teletips += f"\n\n📣  **{get_chat_teletips.title}**\n👤 ├ <i>{get_chat_teletips.members_count} Subscribers</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i>"
+                            edit_message_text_teletips += f"\n\n📣  **{get_chat_teletips.title}**\n👤 ├ <i>{get_chat_teletips.members_count} Subscribers</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i> \n\n ⚡ Powerd By @emMemberCounter_Bot"
                         else:
-                            edit_message_text_teletips += f"\n\n💬  **{get_chat_teletips.title}**\n👤 ├ <i>{get_chat_teletips.members_count} Members</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i>" 
+                            edit_message_text_teletips += f"\n\n💬  **{get_chat_teletips.title}**\n👤 ├ <i>{get_chat_teletips.members_count} Members</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i> \n\n ⚡ Powerd By @emMemberCounter_Bot" 
                         await asyncio.sleep(2)
                     except ValueError:
-                        print(f'ID not found: {CHANNEL_OR_GROUP }. Skipping...')                       
-                edit_message_text_teletips += f"\n\n<i>Automatically refreshes every 15 minutes</i>"
+                        print(f'🤷‍♂️ ID not found: {CHANNEL_OR_GROUP }. Skipping...')                       
+                edit_message_text_teletips += f"\n\n<i>♻ Automatically refreshes every 15 minutes</i>"
                 try:
                     await MemberCounterMeta.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, edit_message_text_teletips, disable_web_page_preview=True)
                 except Exception:
@@ -50,7 +50,7 @@ async def main_MemberCounterMeta():
 
 @MemberCounterMeta.on_message(filters.command("status", "!") & filters.me)
 async def alive(_, message: Message):
-    await message.edit(" Emo MemberCounter is alive!")
+    await message.edit(" Member Counter is alive!")
     await asyncio.sleep(10)
     await message.delete()                   
                         
